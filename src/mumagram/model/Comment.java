8bb913d920bba0bf3e4401e7fddc8aba9cecbeec
Ganzorig Erdenebat <the.ganzorig25@gmail.com>
2018-03-21 13:31:29 -0500

    Added Comment model && repository classes

---
+++ src/mumagram/model/Comment.java
@@ -0,0 +1,64 @@
+package mumagram.model;
+
+import java.time.LocalDate;
+
+public class Comment {
+	private int id;
+	private String comment;
+	private Post post;
+	private User user;
+	private LocalDate createdDate;
+	private LocalDate updatedDate = null;
+	
+	public Comment() {
+		createdDate = LocalDate.now();
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getComment() {
+		return comment;
+	}
+
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+
+	public Post getPost() {
+		return post;
+	}
+
+	public void setPost(Post post) {
+		this.post = post;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public LocalDate getCreatedDate() {
+		return createdDate;
+	}
+
+	public void setCreatedDate(LocalDate createdDate) {
+		this.createdDate = createdDate;
+	}
+
+	public LocalDate getUpdatedDate() {
+		return updatedDate;
+	}
+
+	public void setUpdatedDate(LocalDate updatedDate) {
+		this.updatedDate = updatedDate;
+	}
+}
