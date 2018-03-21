4ea9f1df4cc15634f83d85e1193d30bf5be09386
Ganzorig Erdenebat <the.ganzorig25@gmail.com>
2018-03-21 16:58:15 -0500

    Added email regex

--- src/mumagram/controller/RegisterServlet.java
+++ src/mumagram/controller/RegisterServlet.java
@@ -59,13 +59,13 @@ public class RegisterServlet extends HttpServlet {
 			return;
 		}
 		
-		Pattern usernamePattern = Pattern.compile("^[a-zA-Z0-9_-]{3,}$", Pattern.CASE_INSENSITIVE);
-		Pattern emailPattern = Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
-//		if(!emailPattern.matcher(email).matches()) {
-//			request.setAttribute("error", "Your email address is incorrect!");
-//			request.getRequestDispatcher("/pages/register.jsp").forward(request, response);
-//			return;
-//		}
+		Pattern usernamePattern = Pattern.compile("^[a-zA-Z0-9_-]{3,}$");
+		Pattern emailPattern = Pattern.compile("^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])$");
+		if(!emailPattern.matcher(email).matches()) {
+			request.setAttribute("error", "Your email address is incorrect!");
+			request.getRequestDispatcher("/pages/register.jsp").forward(request, response);
+			return;
+		}
 		if(!usernamePattern.matcher(username).matches()) {
 			request.setAttribute("error", "Your username is incorrect!");
 			request.getRequestDispatcher("/pages/register.jsp").forward(request, response);
